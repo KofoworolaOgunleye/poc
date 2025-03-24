7c9fd68d8324abdb5e5077e7c8e01f2e0fb5d539
Kofoworola Ogunleye <kay16th@yahoo.com>
2025-03-24 17:04:45 +0000

    wip

---
+++ main.tf
@@ -0,0 +1,38 @@
+data "terraform_remote_state" "google_project" {
+  backend = "remote"
+  config = {
+    organization = "oak-national-academy"
+    workspaces = {
+      name = "curr-kofo"
+    }
+  }
+}
+
+variable "tag_id" {
+  description = "Git tag used as a version identifier"
+  type        = string
+}
+
+
+provider "google" {
+  project = "oak-sandbox-wif-bucket"
+  region  = "europe-west2"
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "my-app-${replace(var.tag_id, ".", "-")}"
+  location = "europe-west2"
+  force_destroy = true
+
+  versioning {
+    enabled = false
+  }
+
+  labels = {
+    version = var.tag_id
+  }
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.example.name
+}
