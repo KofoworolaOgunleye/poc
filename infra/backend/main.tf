cfa280f005d6661437c6b1f3b821eacfa6d28663
KofoworolaOgunleye <kay16th@yahoo.com>
2025-04-23 13:54:07 -0400

    Update terraform_plan.yml

---
+++ infra/backend/main.tf
@@ -0,0 +1,35 @@
+data "terraform_remote_state" "google_project" {
+  backend = "remote"
+  config = {
+    organization = "oak-national-academy"
+    workspaces = {
+      name = "curr-kofo"
+    }
+  }
+}
+
+variable "tag_id" {
+  description = "Git tag used as a version identifier"
+  type        = string
+}
+
+
+provider "google" {
+  project = "oak-sandbox-wif-bucket"
+  region  = "europe-west2"
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "my-app-b"
+  location = "europe-west1"
+  force_destroy = true
+
+  versioning {
+    enabled = false
+  }
+
+}
+#test
+output "bucket_name" {
+  value = google_storage_bucket.example.name
+}
